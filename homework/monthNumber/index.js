84d8c27b198b9f743123f764fe779ddc91a1c0ee
alinapolenokit <127287344+alinapolenokit@users.noreply.github.com>
2023-06-26 23:12:08 +0700

    Update index.js

--- homework/monthNumber/index.js
+++ homework/monthNumber/index.js
@@ -1,5 +1,4 @@
-function getMonthByNumber(monthNumber) {
-    var month = [
+let getMonthByNumber = [
     'January',
     'February',
     'March',
@@ -13,6 +12,16 @@ function getMonthByNumber(monthNumber) {
     'November',
     'December'
 ]
-}
 
-console.log(getMonthByNumber);
+console.log(getMonthByNumber[0]);
+console.log(getMonthByNumber[1]);
+console.log(getMonthByNumber[2]);
+console.log(getMonthByNumber[3]);
+console.log(getMonthByNumber[4]);
+console.log(getMonthByNumber[5]);
+console.log(getMonthByNumber[6]);
+console.log(getMonthByNumber[7]);
+console.log(getMonthByNumber[8]);
+console.log(getMonthByNumber[9]);
+console.log(getMonthByNumber[10]);
+console.log(getMonthByNumber[11]);
