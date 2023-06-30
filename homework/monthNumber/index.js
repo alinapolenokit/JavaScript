e842b576b0ffbfbee4c066dae9d77ad3135607b9
alinapolenokit <127287344+alinapolenokit@users.noreply.github.com>
2023-06-30 22:25:27 +0700

    Update index.js

--- homework/monthNumber/index.js
+++ homework/monthNumber/index.js
@@ -1,27 +1,36 @@
-let getMonthByNumber = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-]
+function  getMonthByNumber(monthNumber) {
+    
+        if (monthNumber == 1) {
+            return 'January';
+        } else if (monthNumber == 2) {
+            return 'February';
+        } else if (monthNumber === 3) {
+            return 'March';
+        } else if (monthNumber === 4) {
+            return 'April'; 
+        } else if (monthNumber === 5) {
+            return 'May';
+        } else if (monthNumber === 6) {
+            return 'June';
+        } else if (monthNumber === 7) {
+            return 'July';
+        } else if (monthNumber === 8) {
+            return 'August';
+        } else if (monthNumber === 9) {
+            return 'September';
+        } else if (monthNumber === 10) {
+            return 'October';
+        } else if (monthNumber === 11) {
+            return 'November';
+        } else if (monthNumber === 12) {
+            return 'December';
+        } else {
+        return "некорректный номер месяца";
+        }
+    }
 
-console.log(getMonthByNumber[0]);
-console.log(getMonthByNumber[1]);
-console.log(getMonthByNumber[2]);
-console.log(getMonthByNumber[3]);
-console.log(getMonthByNumber[4]);
-console.log(getMonthByNumber[5]);
-console.log(getMonthByNumber[6]);
-console.log(getMonthByNumber[7]);
-console.log(getMonthByNumber[8]);
-console.log(getMonthByNumber[9]);
-console.log(getMonthByNumber[10]);
-console.log(getMonthByNumber[11]);
+    let monthNumber = ['January', 'February', 'March', 'April'];
+    let х = 0;
+    let text = monthNumber[х+2];
+
+    console.log(text);
